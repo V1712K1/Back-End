f71bc004640dcae9392f8c1455589df8b119fbf9
Vitinho <vitorquintal1@gmail.com>
2021-04-12 16:17:40 +0100

    ficha3_fim_aula12abril

--- fichatrabalho3_app.js
+++ fichatrabalho3_app.js
@@ -26,3 +26,41 @@ function performDownload(){
 }
 
 performDownload(started,update,completed)
+
+// até aqui e a app
+
+// a partir de aqui, ja tem a haver com o array, ou seja, o outro documento
+
+var arrayUtils=require("./fichatrabalho3_array.js"); 
+// require(), e a ligação ao documento que queres
+var array=[12,4,6,88,9,0];
+var otherArray=[7,10];
+
+// nestas tres linhas, defines a ligação, e defines os arrays com valores que queres utilizar
+// daqui para baixo, e so a ver se as funções do outro ficheiro funcionam
+
+
+console.log("O array esta vazio? "+arrayUtils.isEmpty(array));
+console.log("O maximo do array é: "+arrayUtils.maximo(array));
+console.log("O minimo do array é: "+arrayUtils.minimo(array));
+console.log("A media do array é: "+arrayUtils.media(array));
+
+var value=6;
+console.log("O valor é: "+ value +" e o seu index é:"+arrayUtils.index(array,value));
+
+var subArray=arrayUtils.subarray(array,3,5);
+console.log("O sub array é: "+subArray)
+
+var sameSize= arrayUtils.mesmotamanho(array,segarray)
+console.log("Os array sao do mesmo tamanho?: "+sameSize);
+
+var invertedArray=arrayUtils.ordemcontraria(array);
+console.log("O array invertido fica: "+invertedArray)
+
+var swappedArray=arrayUtils.trocaarray(array,0,4);
+console.log("O array alterado é: "+swappedArray)
+
+console.log("O contem o valor "+value+"? "+ arrayUtils.contains(array,value));
+
+var concatArray=arrayUtils.concatenar(array,otherArray);
+console.log("O array concatenado é: "+concatArray);
--- fichatrabalho3_array.js
+++ fichatrabalho3_array.js
@@ -7,28 +7,132 @@ var arrayUtils = {
         return 'Array is undefined';
       }
     },
+    // verificar se o array esta vazio
     
-    max: function (array) {
+    maximo: function (array) {
       if (this.isEmpty(array)) return 'array vazio';
       var max = array[0];
+      // criar variavel para depois comparar
       for (let i = 1; i < array.length; i++) {
+        // percorre o array com o for
         if (array[i] > max) {
           max = array[i];
+          // aqui ele guarda o valor maximo dentro do array
         }
       }
       return max;
     },
+
+    // verifica o valor maximo do array -----------------
     
-    min: function (array) {
+    minimo: function (array) {
       if (this.isEmpty(array)) return 'array vazio';
       var min = array[0];
+      // criar variavel para depois comparar
       for (let i = 0; i < array.length; i++) {
+        // percorre o array com o for
         if (array[i] < min) {
           min = array[i];
+          // aqui ele guarda o valor minimo dentro do array
         }
       }
       return min;
     },
+
+    // verifica o valor minimo do array -----------------
+
+    media: function(array){
+      var sum=0
+      // criar variavel para guardar a soma de todos os valores
+      for (let i = 0; i < array.length; i++) {                    
+          sum+=array[i];
+          // como vês, aqui ele vai somando
+      }
+      var avg=sum/array.length;
+      // esta variavel e que faz a media, poeo valor da soma total a dividir pelo tamanho do array
+      return avg;
+    },
+
+    // faz a media dos valores do array -----------------
+
+    index: function(array,value){
+      for (let i = 0; i < array.length; i++) {
+        // percorre o array
+          if(value==array[i]){
+              return i;
+              // retorna o index
+          }
+      }
+ 
+      return -1;
+    },
+
+    // retorna o valor do index do valor do array selecionado
+
+    subarray: function(array,sIndex,fimIndex){
+      var a=[];
+      for (let i = sIndex; i <= fimIndex; i++) {
+          a.push(array[i]);
+      }
+      return a;
+    },
+
+    // este eu nao percebi mt bem -.-"
+
+    mesmotamanho: function(array,segarray){
+      return array.length==segarray.length
+    },
+
+    // compara dois array, e vê se têm o mesmo tamanho
+
+    ordemcontraria: function(array){
+      var r=[];
+      for (let i = array.length-1; i >= 0; i--) {
+        // o -1 faz com que ele leia o array, da direita para a esquerda
+          r.push(array[i]);
+          // lança para o novo array
+      }
+      return r;
+    },
+
+    // retorna um array, do avesso XD ------------------
+
+    trocaarray: function(array,index1,index2){
+        
+      var val1=array[index1];
+      var val2=array[index2];
+
+      array[index1]=val2;
+      array[index2]=val1;
+
+      return array;
+    },
+
+    // troca dois valores escolhidos -----------
+
+    contains: function(array,value){
+      for (let i = 0; i < array.length; i++) {
+          if(value==array[i]){
+              return true;
+          }
+      }
+      return false;
+
+    },
+
+    // verifica se o valor inserido pertence ao array
+
+    concatenar: function(array,otherArray){
+      var concatArray=array;
+      for (let i = 0; i < otherArray.length; i++) {
+          concatArray.push(otherArray[i])
+        
+      }
+      return concatArray;
+    },
+
+    // juntar dois arrays
+
 };    
     
 module.exports = arrayUtils;
